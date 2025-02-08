9a7f8abb4bdd15b95aeb6d3b68245eefeb8d0bc5
francesco-biscaccia-carrara <francesco.biscaccia.carrara@gmail.com>
2025-02-08 12:34:45 +0100

    Comment added

--- cam_client.c
+++ cam_client.c
@@ -18,10 +18,11 @@
 
 #pragma region DEF_CONST
 
-#define SDL_RENDER 0//(DEBUG PURPOSE)
+#define SDL_RENDER 0//[DEBUG PURPOSE] Enable rendering if set to 1
 
-#define MIN_BUFF 2
-#define REQ_BUFF 4
+#define TRUE 1
+#define MIN_BUFF 2  // Minimum number of buffers required
+#define REQ_BUFF 4  // Requested number of buffers
 #define FRAME_WIDTH 640
 #define FRAME_HEIGHT 480
 #define FILENAME_MAX_LEN 256
@@ -31,8 +32,10 @@ struct buffer{
     size_t length;
 };
 
+// Macro to clear struct memory
 #define CLEAR(x) memset(&(x), 0, sizeof(x))
 
+// Function to handle errors and exit
 static void errno_exit(const char* s){
     fprintf(stderr, "%s error %d, %s\n", s, errno, strerror(errno));
     exit(EXIT_FAILURE);
@@ -42,6 +45,7 @@ static void errno_exit(const char* s){
 
 #pragma region FRAME_PROC_FUN
 
+// Function to render a frame using SDL2 [DEBUG PURPOSE]
 static void render_frame(const void* p, int size_bytes, const struct v4l2_format* v4l_sd2l){
     uint8_t *buf0 = malloc(sizeof(char) * (v4l_sd2l->fmt.pix.sizeimage) * 3);
     decode_sdl2_mjpeg_frame((uint8_t *)p, buf0, size_bytes);
@@ -49,19 +53,24 @@ static void render_frame(const void* p, int size_bytes, const struct v4l2_format
     free(buf0);
 }
 
+// Function to process a single video frame: dequeues, sends, optionally renders, then re-queues
 static int process_frame(int webcam_ds, int socket_ds, struct buffer* buffers,const struct v4l2_format* v4l_sd2l){
     struct v4l2_buffer buf;
     CLEAR(buf);
     buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
     buf.memory = V4L2_MEMORY_MMAP;
 
+    // Dequeue a frame from the buffer
     if (ioctl(webcam_ds, VIDIOC_DQBUF, &buf) == -1) errno_exit("VIDIOC_DQBUF");
 
+    // Send the frame data to the server
     if(send(socket_ds, buffers[buf.index].start, buf.bytesused, 0) == -1) errno_exit("frame_send");
+    
     #if SDL_RENDER
         render_frame(buffers[buf.index].start, buf.bytesused,v4l_sd2l);
     #endif
 
+    // Re-queue the buffer for further capturing
     if (ioctl(webcam_ds, VIDIOC_QBUF, &buf) == -1) errno_exit("VIDIOC_QBUF");
     return 1;
 }
@@ -78,13 +87,13 @@ int main(int argc, char** argv){
     sscanf(argv[1], "%d", &port);
     sscanf(argv[2], "%d", &num_frame);
 
-    //Opening webcam device
-    //REMINDER: Active webcam device on VBox
+    // Open the webcam device
+    // REMINDER: Active webcam device on VirtualBox
     const char* dev_name = "/dev/video0";
     int webcam_ds = -1;
     if((webcam_ds = open(dev_name,O_RDWR | O_NONBLOCK, 0)) == -1) errno_exit(dev_name);
 
-    //Creating socket
+    // Create socket
     struct sockaddr_in sin;
     CLEAR(sin);
     sin.sin_family = AF_INET;
@@ -94,77 +103,61 @@ int main(int argc, char** argv){
     int socket_ds = -1;
     if ((socket_ds = socket(AF_INET, SOCK_STREAM, 0)) == -1) errno_exit("socket");
 
-    //Connection to server localhost:<port>
+    // Connect to server localhost:<port>
     if (connect(socket_ds,(struct sockaddr *)&sin, sizeof(sin)) == -1) errno_exit("connect");
     printf("Connected to %s:%d\n", inet_ntoa(sin.sin_addr),port);
 
-    //Sending filename to server
+    // Send filename to server
     char filename[FILENAME_MAX_LEN];
     sprintf(filename,"Webcam_%d_%d_%d.mjpeg",FRAME_WIDTH,FRAME_HEIGHT,num_frame);
     if (send(socket_ds, filename, strlen(filename), 0) == -1) errno_exit("send_filename");
     printf("Filename %s sent to %s:%d\n",filename,inet_ntoa(sin.sin_addr),port);
 
-    //Initialization of webcam device
-    //1. Querying capabilities
+    // Initialize the webcam device
+    // 1. Query webcam device capabilities
+    // NOTE: V4L2_CAP_READWRITE not work
     struct v4l2_capability cap;
     CLEAR(cap);
 
-    //Querying the devices capabilites
     if(ioctl(webcam_ds,VIDIOC_QUERYCAP,&cap)==-1) errno_exit("VIDIOC_QUERYCAP");
 
-    if (!(cap.capabilities & V4L2_CAP_VIDEO_CAPTURE)){
-        fprintf(stderr, "%s is no video capture device\n",dev_name);
-        exit(EXIT_FAILURE);
-    }
+    if (!(cap.capabilities & V4L2_CAP_VIDEO_CAPTURE)) errno_exit("Not a video capture device");
+    if (!(cap.capabilities & V4L2_CAP_STREAMING)) errno_exit("Streaming I/O not supported");
 
-    if (!(cap.capabilities & V4L2_CAP_STREAMING)){ // Since we use mmap, we'll go for this option
-        fprintf(stderr, "%s does not support streaming i/o\n", dev_name);
-        exit(EXIT_FAILURE);
-    }
-
-    //V4L2_CAP_READWRITE does not work
-
-    //2. Setting format
+    
+    //2. Set video format (MJPEG for VirtualBox compatibility)
     struct v4l2_format my_fmt;
     CLEAR(my_fmt);
 
     my_fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
     my_fmt.fmt.pix.width = FRAME_WIDTH;
     my_fmt.fmt.pix.height = FRAME_HEIGHT;
-    my_fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_MJPEG; //Since in VBOX we have to use MJPEG
+    my_fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_MJPEG;
 
     if (ioctl(webcam_ds, VIDIOC_S_FMT, &my_fmt) == -1) errno_exit("VIDIOC_S_FMT");
     
-    //Initialization of SDL2 (DEBUG PURPOSE)
+    // Initialize SDL2 [DEBUG PURPOSE]
     struct v4l2_format v4l_sd2l; 
     #if SDL_RENDER
         init_render_sdl2(FRAME_WIDTH, FRAME_HEIGHT, 0);
         v4l_sd2l = my_fmt;
     #endif
 
-    //Initilazation of mmap
+    // Initilzie mmap
     struct v4l2_requestbuffers req;
     CLEAR(req);
 
-    req.count = REQ_BUFF; //Asking for REQ_BUFF buffers
+    req.count = REQ_BUFF; 
     req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
     req.memory = V4L2_MEMORY_MMAP;
 
     if (ioctl(webcam_ds, VIDIOC_REQBUFS, &req) == -1) errno_exit("VIDIOC_REQBUFS");
-
-    if (req.count < MIN_BUFF){
-        fprintf(stderr, "Insufficient buffer memory on %s\n",dev_name);
-        exit(EXIT_FAILURE);
-    }
+    if (req.count < MIN_BUFF) errno_exit("Insufficient buffer memory");
 
     struct buffer* buffers = calloc(req.count, sizeof(*buffers));
+    if (!buffers) errno_exit("Out of memory");
 
-    if (!buffers){
-        fprintf(stderr, "Out of memory\n");
-        exit(EXIT_FAILURE);
-    }
-
-    //Mapping device registers to main memory addresses
+    // Map the webcam device registers to main memory addresses
     unsigned int num_buffer;
     for (num_buffer = 0; num_buffer < req.count; ++num_buffer){
         struct v4l2_buffer buf;
@@ -188,7 +181,7 @@ int main(int argc, char** argv){
     }
 
 
-    //Start capturing the frames
+    // Start capturing the frames
     enum v4l2_buf_type type;
     for (size_t i = 0; i < num_buffer; ++i){
         
@@ -205,17 +198,17 @@ int main(int argc, char** argv){
     if (ioctl(webcam_ds, VIDIOC_STREAMON, &type) == -1) errno_exit("VIDIOC_STREAMON");
 
     
-    //Catching the <num_frame> frames required and sending them to the server
-    unsigned int count = num_frame;
-    int i =((count>0)? 0: count); //if <num_frame>=-1 --> acquire all the frames until  the client is interrupted
-    while (i<count){
-        for (;;){
+    // Catch the <num_frame> frames required and sending them to the server
+    // if <num_frame>=-1 --> acquire frames until the client is stopped
+    int count = num_frame;
+    for(int i =((count>0)? 0: count);i<count;i++){
+        while(TRUE){
             fd_set fds;
             FD_ZERO(&fds);
             FD_SET(webcam_ds, &fds);
 
-            //Time interval
-            struct timeval tv={.tv_sec=5,.tv_usec=0};
+            // Time interval for selection
+            struct timeval tv = {.tv_sec=5,.tv_usec=0};
 
             if(select(webcam_ds + 1, &fds, NULL, NULL, &tv)==-1) errno_exit("select");
             #if SDL_RENDER
@@ -226,14 +219,13 @@ int main(int argc, char** argv){
             if (process_frame(webcam_ds,socket_ds,buffers,&v4l_sd2l)) break;
         }
         printf("Frame: %d CATCHED \t SENT to Cserver\n",i+1);
-        i++;
     }
 
-    //Stop capturing the frames
+    // Stop capturing the frames
     type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
     if (-1 == ioctl(webcam_ds, VIDIOC_STREAMOFF, &type)) errno_exit("VIDIOC_STREAMOFF");
       
-    //Freeing the webcam device
+    // Clean-up the webcam device
     for (size_t i = 0; i < num_buffer; ++i)
         if (-1 == munmap(buffers[i].start, buffers[i].length)) errno_exit("munmap");
     free(buffers);
@@ -241,9 +233,9 @@ int main(int argc, char** argv){
         render_sdl2_clean();
     #endif
 
-    //Closing webcam device
+    // Close the webcam device
     if(close(webcam_ds)==-1)  errno_exit(dev_name);
-    //Closing connection to server
+    // Close the socket
     if(close(socket_ds)==-1)  errno_exit("socket_close");
 
     return EXIT_SUCCESS;
--- cam_server.c
+++ cam_server.c
@@ -13,11 +13,14 @@
 
 #pragma region DEF_CONST 
 
-#define BUFFER_SIZE 4096
-#define MAX_FILE_LEN 512
-#define QUEUE_LEN 2
+#define TRUE 1
+#define BUFFER_SIZE 1024    // Buffer size for receiving data
+#define MAX_FILE_LEN 512    // Maximum length for filename
+#define QUEUE_LEN 2         // Max length of connection queue for listen()
+// Macro to clear struct memory
 #define CLEAR(x) memset(&(x), 0, sizeof(x))
 
+// Function to handle errors and exit
 static void errno_exit(const char* s){
     fprintf(stderr, "%s error %d, %s\n", s, errno, strerror(errno));
     exit(EXIT_FAILURE);
@@ -26,16 +29,18 @@ static void errno_exit(const char* s){
 
 #pragma region UTILS
 
+// Function to clean a string by removing non-printable characters
 void clean_string(char *str) {
-    for (int i = 0; i < strlen(str); i++) if (!isprint(str[i])) str[i] = '\0';  // Remove non-printable chars 
+    for (int i = 0; i < strlen(str); i++) if (!isprint(str[i])) str[i] = '\0';
 }
 
+// Function to change the file extension from .mjpeg to .mp4
 void change_extension(const char *input, char *output) {
-    sprintf(output,"%s", input); // Copy input filename
-    strcpy(strrchr(output, '.'), ".mp4"); // Replace extension
+    sprintf(output,"%s", input);
+    strcpy(strrchr(output, '.'), ".mp4");
 }
 
-
+// Function to count frames by detecting MJPEG start marker (0xFF 0xD8)
 static void count_frame(int* frame_count, const char* buffer,int rec_bytes){
     char prev_byte=0;
     for (int i = 0; i < rec_bytes; i++) {
@@ -58,14 +63,15 @@ int main(int argc, char *argv[]){
     sscanf(argv[1], "%d", &port);
     if(argc>2 && !strcmp(argv[2],"-c")) convert=1;
     
-    //Creating socket
+    // Create socket
     int socket_ds=-1;
     if ((socket_ds = socket(AF_INET, SOCK_STREAM, 0)) == -1) errno_exit("socket");
 
+    // Enable address reuse
     int reuse = 1;
     if (setsockopt(socket_ds, SOL_SOCKET, SO_REUSEADDR, (const char*)&reuse, sizeof(reuse)) < 0) errno_exit("setsockopt(SO_REUSEADDR) failed");
 
-    //Binding process to localhost:<port> 
+    // Bind socket to localhost:<port> 
     struct  sockaddr_in sin;
     CLEAR(sin);
     sin.sin_family = AF_INET;
@@ -74,28 +80,30 @@ int main(int argc, char *argv[]){
 
     if(bind(socket_ds, (struct sockaddr *) &sin, sizeof(sin)) == -1) errno_exit("bind");
 
-    //Listening to incoming connection 
+    // Start listening for incoming connections 
     if(listen(socket_ds, QUEUE_LEN) == -1) errno_exit("listen");
     printf("Listening to port %d...\n",port);
 
-    for(;;){
+    while(TRUE){
         int client_ds=-1;
         struct  sockaddr_in sClient;
         int sAddrLen = sizeof(sClient);
         if ((client_ds = accept(socket_ds, (struct sockaddr *) &sClient, &sAddrLen)) == -1) errno_exit("accept");
         printf("Connection received from %s\n", inet_ntoa(sClient.sin_addr));
 
-        //Reading the filename received from the client
+        // Read the filename from the client
         char filename[MAX_FILE_LEN];
         read(client_ds, filename, MAX_FILE_LEN);
         clean_string(filename);
         printf("Filename: %s\n", filename);
-        int file_ds = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 
+        // Open file for writing received data
+        int file_ds = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
         char *buffer = calloc(BUFFER_SIZE, sizeof(char));
         int frame_count=0;
-        int rec_bytes;  
-        //Receiving frames from client and writing them on file <filename>
+        int rec_bytes; 
+
+        // Receive frames from client and write them to file
         while ((rec_bytes = recv(client_ds, buffer, BUFFER_SIZE-1, 0)) > 0) {
             write(file_ds, buffer, rec_bytes);
             
@@ -103,9 +111,9 @@ int main(int argc, char *argv[]){
             count_frame(&frame_count,buffer,rec_bytes);
         }
         free(buffer);
-
         printf("File saved successfully. Frames received: %d\n",frame_count);
-        //Command to convert MJPEG to MP4
+
+       // Convert MJPEG to MP4 if <-c> flag is set
         if(convert){
             char output_filename[MAX_FILE_LEN];
             change_extension(filename, output_filename);
@@ -116,12 +124,9 @@ int main(int argc, char *argv[]){
             printf("Conversion to MP4 complete: %s\n", output_filename);
         }
         
-
-        //ffmpeg -i %s -c:v libx264 -preset fast -crf 23 %s, filname, output filename
-        
-        //Closing file
+        // Close file
         close(file_ds);
-        //Closing client connection (server-side)
+        // Close client connection
         close(client_ds);
     }
 
